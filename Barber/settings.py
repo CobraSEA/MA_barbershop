985a024677f69f62e6f359500e3208f645a715e7
Yevhen Samoilenko <eugene.samoilenko@gmail.com>
2020-11-03 22:23:04 +0200

    add static_root into settings

--- Barber/settings.py
+++ Barber/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from .develop_app import *
 import psycopg2 as psycopg2
@@ -117,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
